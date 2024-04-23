ff002cc8f086a360f6b549ebd21590b57837b480
Alex Miguel <alexm@esparta.io>
2024-04-22 22:04:31 -0300

    configured svg types

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
 import { WithTranslation, withTranslation } from 'react-i18next';
 import * as S from './styled';
+import Logo from 'assets/images/logo.svg';
 
 const Header = ({ t }: WithTranslation) => {
     return (
         <S.HeaderContainer>
             <S.HeaderLogo>{t('header.title')}</S.HeaderLogo>
+            <Logo />
         </S.HeaderContainer>
     );
 };
